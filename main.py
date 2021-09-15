ff5727ff79e8d0b14a2fdc65423575e1c2e9633b
이주원 <60950583+Su-per@users.noreply.github.com>
2021-09-15 14:42:05 +0900

    Update main.py
    기본 코드

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import discord
 
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
+    print("------")
+
+
+@client.event
+async def on_message(message):
+    cmd = message.content.split(" ")[0]
+    args = message.content.split(" ")[1:]
+
+    if message.content.startswith("!ping"):
+        await message.channel.send("pong")
+
+
+client.run("token")
