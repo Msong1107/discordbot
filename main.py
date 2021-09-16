a3c7da90ebe73a2fc7ab33025f17628b43728f9a
시훈 <81404026+sihun96@users.noreply.github.com>
2021-09-16 09:19:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,13 @@ async def on_message(message):
     cmd = message.content.split(" ")[0]
     args = message.content.split(" ")[1:]
 
-    if message.content.startswith("!ping"):
-        await message.channel.send("pong")
+    if message.content.startswith("$생성 ec2"):
+       id = message.author.id
+        qnum = 1
+        instance_name = args[1]
+        data["id"] =id
+        data["qnum"]=qnum
+        data["instance_name"] = instance_name
 
 
 client.run("token")
